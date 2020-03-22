aa1777071a153c1666a8d1229efacf915b898899
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-03-22 11:59:37 +0100

    add

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
-const express = require('express');
+﻿const express = require('express');
 const routes = require('./src/routes');
+const cors = require('cors');
+const path = require('path');
+
 const db = require('./config/db')();
 
 const app = express();
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, 'uploads')))
 app.use(routes);
 
 
